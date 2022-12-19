2797fa3878b02d34ac2402413af3b15fe11ff1a0
Jake Kadir <sc19jcrk@leeds.ac.uk>
2022-12-19 22:32:52 +0000

    Minor refactor

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from routers.user import router as user_router
 from routers.relationships import router as relationship_router
-from database import SessionLocal, engine
+from database import engine
 import models
 
 models.Base.metadata.create_all(bind=engine)
