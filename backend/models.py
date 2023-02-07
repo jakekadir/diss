5410155b37814d2ad65c8e7ab66b40f254197076
Jake Kadir <sc19jcrk@leeds.ac.uk>
2023-02-07 14:56:01 +0000

    Added route to upload data to DB

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from sqlalchemy import Integer, Column, String, Boolean, Enum, UniqueConstraint, ForeignKey, DateTime, Float
+from sqlalchemy import Integer, Column, String, Boolean, Enum, UniqueConstraint, ForeignKey, DateTime, Float, Interval
 from sqlalchemy.orm import relationship
 import enum
 from database import Base
@@ -40,12 +40,13 @@ class Recipe(Base):
     __tablename__ = "recipes"
 
     id = Column(Integer, primary_key=True)
-    description = Column(String(300))
-    servings = Column(String(100))
-    prep_time = Column(DateTime)
-    total_time = Column(DateTime)
+    name = Column(String(2000))
+    description = Column(String(7000))
+    servings = Column(String(2000))
+    prep_time = Column(Interval)
+    total_time = Column(Interval)
     date_published = Column(DateTime)
-    category = Column(String(100))
+    category = Column(String(2000))
     aggregated_rating = Column(Float)
     review_count = Column(Float)
     calories = Column(Float)
@@ -58,7 +59,7 @@ class Recipe(Base):
     sugar = Column(Float)
     protein = Column(Float)
     servings = Column(Float)
-    recipe_yield = Column(Float)
+    recipe_yield = Column(String(2000))
         
     author = Column(Integer, ForeignKey("users.id"))
     
@@ -68,38 +69,39 @@ class Recipe(Base):
     keywords = relationship("RecipeKeyword", cascade="all, delete, delete-orphan", lazy="dynamic")
 
 class RecipeStep(Base):
-    __tablename__ = "recipestep"
+    __tablename__ = "recipesteps"
     id = Column(Integer, primary_key=True)
     recipe = Column(Integer, ForeignKey("recipes.id"))
-    text = Column(String(300))
+    text = Column(String(2000))
     index = Column(Integer)
     # ensures each recipe / index pair is unique
     UniqueConstraint("recipe", "index", name="unique_recipe_item")
 
 class RecipeIngredient(Base):
-    __tablename__ = "recipeingredient"
+    __tablename__ = "recipeingredients"
     id = Column(Integer, primary_key=True)
     recipe = Column(Integer, ForeignKey("recipes.id"))
-    text = Column(String(300))
+    text = Column(String(2000))
     index = Column(Integer)
+    quantity = Column(String(2000))
 
     # ensures each recipe / index pair is unique
     UniqueConstraint("recipe", "index", name="unique_recipe_ingredient_item")
-    quantity = Column(String(50))
 
 class RecipeImage(Base):
-    __tablename__ = "recipeimage"
+    __tablename__ = "recipeimages"
     id = Column(Integer, primary_key=True)
     recipe = Column(Integer, ForeignKey("recipes.id"))
-    url = Column(String(100))
+    url = Column(String(2000))
     index = Column(Integer)
     
     # ensures each recipe / index pair is unique
     UniqueConstraint("recipe", "index", name="unique_recipe_image_url")
 
 class RecipeKeyword(Base):
-    __tablename__ = "recipekeyword"
+    __tablename__ = "recipekeywords"
     id = Column(Integer, primary_key=True)
     recipe = Column(Integer, ForeignKey("recipes.id"))
-    keyword = Column(String(100))
+    keyword = Column(String(2000))
+    index = Column(Integer)
 
--- backend/routers/recipe.py
+++ backend/routers/recipe.py
@@ -3,15 +3,93 @@ from fastapi import APIRouter, HTTPException, status, Depends, Form
 import crud
 from dependencies import get_db
 from models import Recipe, RecipeImage, RecipeIngredient, RecipeKeyword, RecipeStep
+from datetime import timedelta
+import re
 
 import ast
 import pandas as pd
 
 router = APIRouter()
-# load dataframe
-recipes = pd.read_csv("../data/recipes.csv")
+
+def time_parser(time_str: str) -> timedelta:
+    
+    # regex str
+    time_extraction: str = "(([0-9]+)H)?(([0-9]+)M)?"
+    
+    # perform search
+    search = re.search(time_extraction, time_str)
+    
+    # if the pattern matches
+    if search:
+        
+        # create time object with group 2 as hour and group 4 as minute
+        hours = int(search.group(2)) if search.group(2) is not None else 0
+        minutes = int(search.group(4)) if search.group(4) is not None else 0
+        
+        # if more than 24 hours, convert to days
+        days = hours // 24
+        hours -= days * 24
+        
+        time_val = timedelta(days, hours, minutes, 0)
+        return time_val
+    
+    else:
+        return None
+    
+
+def load_data():
+    # load dataframe
+    recipes = pd.read_csv("../data/recipes.csv")
+
+    # parse columns with multiple values per column
+    multi_cols = [
+        "Images",
+        "RecipeInstructions",
+        "RecipeIngredientParts",
+        "RecipeIngredientQuantities",
+        "Keywords"
+    ]
+    
+    for col in multi_cols:
+
+        # drop malformed rows
+        recipes = recipes.drop(recipes[recipes[col].str[:2] != "c("].index)
+
+        # strip the leading c from relevant rows
+        recipes[col] = recipes[col].str[1:]
+        
+        # replace NA values with empty string
+        regs = ["(\()(NA)(,)","(, )(NA)(,)", "(, )(NA)(\))"]
+        for reg in regs:
+            recipes["RecipeIngredientQuantities"] = recipes["RecipeIngredientQuantities"].str.replace(reg, r'\1""\3', regex=True)
+
+    # fill NaNs
+    str_cols = recipes.select_dtypes(include=['object']).columns
+    recipes.loc[:, str_cols] = recipes.loc[:, str_cols].fillna("")
+
+    float_cols = recipes.select_dtypes(include=['float64']).columns
+    recipes.loc[:, float_cols] = recipes.loc[:, float_cols].fillna(0.0)
+
+    int_cols = recipes.select_dtypes(include=['int64']).columns
+    recipes.loc[:, int_cols] = recipes.loc[:, int_cols].fillna(0)
+    
+    # correctly format times
+    time_cols = ["PrepTime", "CookTime", "TotalTime"]
+    
+    for time_col in time_cols:
+        # strip "PT" prefix
+        recipes[time_col] = recipes[time_col].str.slice(start=2)
+        recipes[time_col] = recipes[time_col].apply(time_parser)
+        
+    # format dates
+    recipes["DatePublished"] = pd.to_datetime(recipes["DatePublished"])
+
+    return recipes
+
 
 def parseTupleFunc(tupleStr):
+    
+    parsed_tuple = []
 
     try:
         return ast.literal_eval(tupleStr)
@@ -50,18 +128,17 @@ async def populate_recipes(db: Session = Depends(get_db)):
     RecipeInstructions (multiple)
     """
 
-    # for each row
-    for recipe in recipes[:5].iterrows():
+    def saveRecipe(recipe):
 
-        print(recipe)
         # create recipe record
         recipe_dict = {
+            "name" : recipe["Name"],
             "description" : recipe["Description"],
-            "servings" : recipe["Servings"],
+            "servings" : recipe["RecipeServings"],
             "prep_time" : recipe["PrepTime"],
             "total_time" : recipe["TotalTime"],
             "date_published" : recipe["DatePublished"],
-            "category" : recipe["Category"],
+            "category" : recipe["RecipeCategory"],
             "aggregated_rating" : recipe["AggregatedRating"],
             "review_count" : recipe["ReviewCount"],
             "calories" : recipe["Calories"],
@@ -77,40 +154,56 @@ async def populate_recipes(db: Session = Depends(get_db)):
             "recipe_yield" : recipe["RecipeYield"]
         }
 
-        recipe = Recipe(recipe_dict)
+        recipe_record = Recipe(**recipe_dict)
 
         # create image record
         image_urls = parseTupleFunc(recipe["Images"])
         for i in range(len(image_urls)):
 
-            image = RecipeImage({"url" : image_urls[i], "index" : i})
+            image = RecipeImage(**{"url" : image_urls[i], "index" : i})
 
-            db.images.append(image)
+            recipe_record.images.append(image)
 
         # create steps record
         steps = parseTupleFunc(recipe["RecipeInstructions"])
         for i in range(len(steps)):
 
-            step = RecipeStep({"text" : steps[i], "index" : i})
+            step = RecipeStep(**{"text" : steps[i], "index" : i})
 
-            db.images.append(step)
+            recipe_record.steps.append(step)
 
         # create ingredients record
-        ingredients = parseTupleFunc(recipe["RecipeIngredients"])
-        for i in range(len(ingredients)):
+        ingredients = parseTupleFunc(recipe["RecipeIngredientParts"])
+        ingredient_quantities = parseTupleFunc(recipe["RecipeIngredientQuantities"])
+        for i, (ingredient, quantity) in enumerate(zip(ingredients, ingredient_quantities)):
 
-            ingredient = RecipeIngredient({"text" : ingredients[i], "index" : i})
+            ingredient = RecipeIngredient(**{"text" : ingredient, "index" : i, "quantity" : quantity})
 
-            db.ingredients.append(ingredient)
+            recipe_record.ingredients.append(ingredient)
 
         # create keywords record
-        keywords = parseTupleFunc(recipe["RecipeKeyword"])
+        keywords = parseTupleFunc(recipe["Keywords"])
         for i in range(len(keywords)):
 
-            keyword = RecipeKeyword({"text" : keywords[i], "index" : i})
+            keyword = RecipeKeyword(**{"keyword" : keywords[i], "index" : i})
 
-            db.keywords.append(keyword)
+            recipe_record.keywords.append(keyword)
+            
+        db.add(recipe_record)
 
-        db.add(recipe)
-    db.commit()
-    db.refresh(recipe)
+    print("LOADING DATA")
+    recipes = load_data()
+    
+    print("BEGINNING TO CREATE RECIPE RECORDS")
+    print("TOTAL RECIPES: ", len(recipes))
+    for i in range(69,(len(recipes) // 1000) + 1):
+        print(f"RECIPES {i * 1000} TO {(i + 1 ) * 1000 - 1}")
+        recipes.iloc[i * 1000: ((i+1) * 1000) - 1].apply(saveRecipe, axis=1)
+        db.commit()
+        print(f"COMITTED RECIPES {i * 1000} TO {(i + 1 ) * 1000 - 1}\n")
+    # recipes.apply(saveRecipe, axis=1)
+    
+    print("ABOUT TO COMMIT DB")
+
+    
+    print("COMMITTED DB, COMPLETE")
